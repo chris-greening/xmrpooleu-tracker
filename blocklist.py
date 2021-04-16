5a280c4231362b9565dd5d2d509f8d82444bc6a8
chris-greening <chrisgreening3@gmail.com>
2021-04-16 19:49:25 -0400

    add: added scroll for 1000 iterations

--- blocklist.py
+++ blocklist.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from selenium.webdriver import Chrome
 from bs4 import BeautifulSoup
@@ -12,6 +13,28 @@ class BlockList:
         self.chrome = Chrome(executable_path=CHROMEDRIVER)
         self.chrome.get(self.url)
 
+        self._load_all_blocks()
+
+    def _load_all_blocks(self):
+        """Load list of Block instances"""
+        self.table = self.chrome.find_element_by_class_name("table")
+        self.blocks = []
+        scroll = True
+
+        # Scroll to the bottom
+        i = 0
+        while scroll:
+            time.sleep(.3)
+            self.load_button = self.chrome.find_element_by_id("loadMoreBlocks")
+            self.load_button.click()
+            if i == 1000:
+                scroll = False
+            i += 1
+            print(i)
+
+class Block:
+    def __init__(self, soup: BeautifulSoup) -> None:
+        pass
 
 if __name__ == "__main__":
     blocklist = BlockList("https://web.xmrpool.eu/xmr-pool-blocks.html")
